af0e86b5bdcba206eae122470de94c2a8b5d60ac
Toma Simena <toma.simena.1@gmail.com>
2022-04-25 21:16:22 +0200

    Edited axios.js file

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL : 'http://localhost:8080/api',
+    baseURL : 'https://mylibraryemt.herokuapp.com/',
     headers : {
         'Access-Control-Allow-Origin' : '*'
     }
